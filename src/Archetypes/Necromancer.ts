a73552c7f89f14fbc261662545ad121d854cfcf2
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-09-23 08:28:26 -0400

    Feat: Adiciona as classes herdeiras e seus atributos - REQ05

--- src/Archetypes/Necromancer.ts
+++ src/Archetypes/Necromancer.ts
@@ -1,1 +1,25 @@
-import Archetype from "./Archetype";
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Necromancer extends Archetype {
+  static numberInstances = 0;
+  private _energyType: EnergyType;
+
+  constructor(_name: string) {
+    super(_name);
+    Necromancer.incrementArchetypeInstances();
+    this._energyType = 'mana';
+  }
+
+  static incrementArchetypeInstances() {
+    this.numberInstances += 1;
+  }
+
+  static createdArchetypeInstances() {
+    return this.numberInstances;
+  } 
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
