d78d5f76c382edc0a5f4c0ac2145bbbd956ebe82
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-09-23 09:32:03 -0400

    Refator: Adiciona a interface SimpleFighter altera os arquivos Figther e Character - REQ08

--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,6 @@
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import utils from './utils';
 
@@ -74,7 +74,7 @@ export default class Character {
     return this._lifePoints;
   }
 
-  attack(enemy: Fighter): void {
+  attack(enemy: SimpleFighter): void {
     enemy.receiveDamage(this._strength);
   }
 
--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,15 +1,9 @@
+import SimpleFighter from './SimpleFighter';
 import Energy from '../Energy';
 
-export default interface Fighter extends Fighter2 {
+export default interface Fighter extends SimpleFighter {
   defense: number,
   energy?: Energy,
   special(enemy: Fighter): void,
   levelUp(): void,
-}
-
-interface Fighter2 {
-  lifePoints: number,
-  strength: number,
-  attack(enemy: Fighter2): void,
-  receiveDamage(attackPoints: number): void,
 }
