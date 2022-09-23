317198ce2e3d848ea9f866c95ac8abbb5e17f8ef
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-09-23 07:30:14 -0400

    Feat: Adiciona a classe Archetype e seus atributos, faltando apenas o energyType do REQ03 faltante - REQ04

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,27 @@
+// A classe Archetype deve ter os atributos privados: name, special, cost, que serão inicializados em seu construtor;
+export default abstract class Archetype {
+  private _special: number;
+  private _cost: number;
+
+  constructor(private _name: string) { // name deve ser recebido como parâmetro e inicializado no construtor;
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  // Os atributos da classe Archetype podem ser lidos, mas não podem ser alterados;
+  get name() {
+    return this._name;
+  }
+
+  get special() {
+    return this._special;
+  }
+
+  get cost() {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances(): number { // A classe Archetype deve ter um método estático chamado createdArchetypeInstances que retorna um number
+    throw new Error('Not implemented');
+  }
+}
